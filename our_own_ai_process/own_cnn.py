6fcbaad17c7c19556808917579b9a1fb63dd4bcc
abiyyumufti <akang.mufti@gmail.com>
2021-07-05 00:52:53 +0200

    Auto stash before merge of "master" and "origin/master"

--- our_own_ai_process/own_cnn.py
+++ our_own_ai_process/own_cnn.py
@@ -1,9 +1,97 @@
+import numpy as np
+import pandas as pd
+from imutils import paths
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer, StandardScaler
+
 from ai_process import AiProcess
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
+from tensorflow.keras.optimizers import SGD
+import tensorflow as tf
+
+from preprocessing import SimpleDatasetLoader, ImageResizer
+from pyimagesearch.preprocessing import ImageToArrayPreprocessor
 
 
 class OurCNN(AiProcess):
-    def __init__(self):
+    def __init__(self, height, width, depth, learning_rate):
         super().__init__()
+        # !!! Creating Model !!!
+        # initialize the model along with the input shape to be "channels last"
+        self.model = Sequential()
+        self.classes = 2
+        input_shape = (height, width, depth)
+        # if we are using "channels first", update the input shape
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+        # define the first (and only) CONV => RELU layer
+        self.model.add(Conv2D(32, (3, 3), padding="same", input_shape=input_shape))
+        self.model.add(Activation("relu"))
+        # softmax classifier
+        self.model.add(Flatten())
+        self.model.add(Dense(self.classes))
+        self.model.add(Activation("softmax"))
+        opt = SGD(learning_rate=learning_rate)
+        self.model.compile(loss='binary_crossentropy', optimizer=opt, metrics=["accuracy"])
+        print(self.model.summary())
+
+    def fit(self, data, test_size):
+        data = data.astype("float") / 255.0
+        (self.train_value, self.test_value, self.train_labels, self.test_labels) = train_test_split(data, labels, test_size=test_size, random_state=42)
+        le = LabelBinarizer()
+        self.train_labels = le.fit_transform(self.train_labels)
+        self.test_labels = le.fit_transform(self.test_labels)
+        self.train_labels = np.hstack((self.train_labels, 1 - self.train_labels))
+        self.test_labels = np.hstack((self.test_labels, 1 - self.test_labels))
+
+    def train(self, batch_size, epochs):
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.H = self.model.fit(x=self.train_value, y=self.train_labels,
+                                validation_data=(self.test_value, self.test_labels),
+                                batch_size=batch_size, epochs=epochs, verbose=1)
+
+    def plot_history(self):
+        plt.style.use("ggplot")
+        plt.figure()
+        plt.plot(np.arange(0, self.epochs), self.H.history["loss"], label="train_loss")
+        plt.plot(np.arange(0, self.epochs), self.H.history["val_loss"], label="val_loss")
+        plt.plot(np.arange(0, self.epochs), self.H.history["accuracy"], label="train_acc")
+        plt.plot(np.arange(0, self.epochs), self.H.history["val_accuracy"], label="val_acc")
+        plt.title("Training Loss and Accuracy")
+        plt.xlabel("Epoch #")
+        plt.ylabel("Loss/Accuracy")
+        plt.legend()
+        plt.show()
+
+    def review(self):
+        pass
 
     def predict(self, test=None):
-        pass
+        if test is not None:
+            self.test_value = test
+
+        predictions = self.model.predict(self.test_value, batch_size=self.batch_size)
+        self.prediction.append(predictions.argmax(axis=1))
+        return self.prediction
+
+
+if __name__ == '__main__':
+    # my_data = pd.read_csv(r"D:\VDKICombBrush\800ImagesFeatures.csv")
+    imagePaths = list(paths.list_images(r"D:/VDKICombBrush/resources/"))
+    sp = ImageResizer(32, 32)
+    iap = ImageToArrayPreprocessor()
+    sdl = SimpleDatasetLoader(preprocessors=[sp, iap])
+    (data, labels) = sdl.load(imagePaths, verbose=500)
+    cnn = OurCNN(32,32,3,0.01)
+    cnn.fit(data, 0.1)
+    cnn.train(30, 100)
+    cnn.plot_history()
+    cnn.predict()
+    cnn.plot_history()
--- our_own_ai_process/own_knn.py
+++ our_own_ai_process/own_knn.py
@@ -1,9 +1,44 @@
+from scipy.spatial.distance import minkowski
+from sklearn.preprocessing import StandardScaler
+
 from ai_process import AiProcess
+from collections import Counter
+import pandas as pd
 
 
 class OurKNearestNeighbour(AiProcess):
-    def __init__(self):
+    def __init__(self, k, algorithm="euclidean"):
         super().__init__()
+        self.K = k
+        distance_type = {"manhattan": 1, "euclidean": 2}
+        self.p = distance_type[algorithm.lower()]
 
     def predict(self, test=None):
-        pass
+        self.train_labels = self.train_data.labels
+        self.train_value = self.train_data.drop('labels', axis=1)
+        if test is None:
+            self.test_labels = self.test_data.labels
+            self.test_value = self.test_data.drop('labels', axis=1)
+        else:
+            self.test_value = test
+        scaler = StandardScaler()
+        self.train_value = scaler.fit_transform(self.train_value)
+        self.test_value = scaler.transform(self.test_value)
+        for i, test_point in enumerate(self.test_value):
+            distance = []
+            for each_train_val in self.train_value:
+                distance.append(minkowski(test_point, each_train_val, self.p))
+            df_dists = pd.DataFrame(data=distance, columns=["dist"],
+                                    index=self.train_labels.index)
+            df_nn = df_dists.sort_values(by=['dist'], axis=0)[:self.K]
+            counter = Counter(self.train_labels[df_nn.index])
+            self.prediction.append(counter.most_common()[0][0])
+        return self.prediction
+
+
+if __name__ == '__main__':
+    my_data = pd.read_csv(r"D:\VDKICombBrush\800ImagesFeatures.csv")
+    OKKN = OurKNearestNeighbour(5, "euclidean")
+    OKKN.fit(my_data, 0.25)
+    OKKN.predict()
+    print(*OKKN.review())
