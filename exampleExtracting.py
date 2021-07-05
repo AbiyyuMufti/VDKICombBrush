cd513269c3cd58a743cf928cecaf33f4060c511e
abiyyumufti <akang.mufti@gmail.com>
2021-07-05 22:45:14 +0200

    More Efficient Image Feature Extraction

--- exampleExtracting.py
+++ exampleExtracting.py
@@ -1,24 +1,23 @@
-
 from imutils import paths
-
-from preprocessing import FeatureExtraction, ImageResizer
+from preprocessing import ImageResizer, FeatureExtraction
 from preprocessing import SimpleDatasetLoader
-
-args = {
-	"dataset": "resources",
-	"neighbors": 1,
-	"jobs": -1
-}
+import time
 
 
-print("[INFO] loading images...")
-imagePaths = list(paths.list_images(args["dataset"]))
-# initialize the image preprocessor, load the dataset from disk,
-# and reshape the data matrix
+imagePaths = list(paths.list_images("resources/"))
+# initialize the image preprocessor, load the dataset from disk, and reshape the data matrix
 sp = ImageResizer(256, 256)
-f_ext = FeatureExtraction("800ImagesFeatures.csv")
+f_ext = FeatureExtraction("ImagesFeatures2.csv")
 sdl = SimpleDatasetLoader(preprocessors=[sp, f_ext])
 
+
 if __name__ == '__main__':
-	(data, labels) = sdl.load(imagePaths, verbose=10)
-	f_ext.extract_to_table(labels, "w")
+	print("[INFO] loading images...")
+	start1 = time.perf_counter()
+	(data, labels) = sdl.load(imagePaths, verbose=100)
+	end1 = time.perf_counter()
+	start2 = time.perf_counter()
+	df = f_ext.extract_to_table(labels)
+	end2 = time.perf_counter()
+	print("Loading Images:", end1 - start1)
+	print("Create table Images:", end2 - start2)
--- preprocessing/image_preprocessing.py
+++ preprocessing/image_preprocessing.py
@@ -1,8 +1,10 @@
+from preprocessing.features_extraction import CornersDetection, \
+	CornerHarrisDetection, ContoursDetection, thresholding_image
 from sklearn.preprocessing import LabelEncoder
-from tensorflow.keras.preprocessing.image import img_to_array
 import csv
 import cv2
 import numpy as np
+import pandas as pd
 
 
 class ImageResizer:
@@ -23,127 +25,52 @@ class FeatureExtraction:
 	def __init__(self, csv_files):
 		self.csv_data = csv_files
 		self.list_of_features = []
+		self.data_frame = None
 
 	def preprocess(self, image):
-		img_copy = image.copy()
 		img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		dark_bright_threshold = 200
 		mean_of_gray_image = np.mean(img_gray)
-		if mean_of_gray_image < dark_bright_threshold:
+
+		if mean_of_gray_image < 180:
 			# Image is dark
+			# print("dark image")
 			contour_recognition_threshold = 120
 		else:
+			# print("bright image")
 			# Image is bright
-			contour_recognition_threshold = 200
+			contour_recognition_threshold = 220
+		img_thresh = thresholding_image(img_gray, contour_recognition_threshold)
 
-		# thresholding
-		img_thresh = self.thresholding_image(img_gray, contour_recognition_threshold)
-		# get all contours
-		cnt, contours = self.get_biggest_contour(img_thresh)
-
-		contour_area = cv2.contourArea(cnt)
-		# draw the biggest contour
-		to_show_contour = img_copy.copy()
-		cv2.drawContours(to_show_contour, cnt, -1, (0, 255, 0), 2, cv2.LINE_AA)
-
-		# make rectangle that boxing the biggest contour
-		rect = cv2.minAreaRect(cnt)
-		rect_area = rect[1][0] * rect[1][1]
-		box = cv2.boxPoints(rect)
-		box = np.int0(box)
-
-		# draw the box
-		to_show_box = img_copy.copy()
-		cv2.drawContours(to_show_box, [box], 0, (0, 0, 255), 2)
-
-		# make hull that surround the biggest contour
-		hull = cv2.convexHull(cnt)
-		hull_area = cv2.contourArea(hull)
-
-		# draw the hull
-		to_show_hull = img_copy.copy()
-		cv2.drawContours(to_show_hull, [hull], 0, (255, 0, 0), 2)
-
-		# calculate the perimeters of the biggest contour
-		contour_perimeters = cv2.arcLength(cnt, True)
-		approx = cv2.approxPolyDP(cnt, 0.001 * contour_perimeters, True)
-
-		#  show the approx perimeters
-		to_show_approx = img_copy.copy()
-		cv2.drawContours(to_show_approx, [approx], -1, (0, 0, 255), 3)
-
-		# calculate approximation area of the biggest contour
-		approximation_area = cv2.contourArea(approx)
-
-		corners = self.get_corners(img_copy, img_gray)
-
-		amount_h_corners = self.get_harris_corners(img_copy, img_gray)
-
-		ret = {
-			"contour_points": len(cnt),
-			"amount_contours": len(contours),
-			"rect_area": rect_area,
-			"hull_area": hull_area,
-			"approximation_area": approximation_area,
-			"contour_perimeters": contour_perimeters,
-			"corners": corners,
-			"harris_corners": amount_h_corners,
-			"ratio_wide_length": rect[1][0] / rect[1][1],
-			"contour_length_area_ratio": contour_perimeters / contour_area,
-			"contour_length_rect_area_ratio": contour_perimeters / rect_area,
-			"contour_length_hull_area_ratio": contour_perimeters / hull_area,
-			"contour_rect_length_ratio": contour_perimeters / (2 * (rect[1][0] + rect[1][1])),
-			"contour_hull_length_ratio": contour_perimeters / cv2.arcLength(hull, True),
-			"extent": contour_area / rect_area,
-			"solidity": contour_area / hull_area,
-			# ""
-			"hull_rectangle_ratio": hull_area / rect_area,
+		a = CornersDetection(img_gray)
+		b = CornerHarrisDetection(img_gray)
+		c = ContoursDetection(img_thresh)
+		features_extraction = [a, b, c]
+		for F in features_extraction:
+			F.preprocess()
+		features_extracted = {
+			'n_corner': a.number_of_corners,
+			'n_h_corner': b.number_of_corners,
+			'n_contour': c.contours_numbers,
+			'a_rect': c.rect_area,
+			'a_hull': c.hull_area,
+			'a_approx': c.approximation_area,
+			'l_perimeters': c.contour_perimeters,
+			'wide/length': c.wide / c.length,
+			'perim/a_rect': c.contour_perimeters / c.rect_area,
+			'perim/a_hull': c.contour_perimeters / c.hull_area,
+			'perim/a_appx': c.contour_perimeters / c.approximation_area,
+			'corner/a_rect': a.number_of_corners / c.rect_area,
+			'corner/a_hull': a.number_of_corners / c.hull_area,
+			'corner/a_appx': a.number_of_corners / c.approximation_area,
+			'corner/l_perim': a.number_of_corners / c.contour_perimeters,
+			'h_corner/a_rect': b.number_of_corners / c.rect_area,
+			'h_corner/a_hull': b.number_of_corners / c.hull_area,
+			'h_corner/a_appx': b.number_of_corners / c.approximation_area,
+			'h_corner/l_perim': b.number_of_corners / c.contour_perimeters,
+			'extent': c.approximation_area / c.rect_area,
+			'solidity': c.approximation_area / c.hull_area
 		}
-
-		self.list_of_features.append(ret)
-
-		return image
-
-	def get_biggest_contour(self, img_thresh):
-		contours, _ = cv2.findContours(img_thresh, cv2.RETR_TREE, cv2.cv2.CHAIN_APPROX_NONE)
-		# exclude the contour of the image frame
-		im_boundary = (img_thresh.shape[0] - 1) * (img_thresh.shape[1] - 1)
-		areas = [cv2.contourArea(ar) for ar in contours]
-		cnt = [x for x in areas if x != im_boundary]
-		# get the biggest contour
-		cnt = contours[areas.index(max(cnt))]
-		return cnt, contours
-
-	def get_corners(self, img_copy, img_gray):
-		# get corners from good feature to track
-		corners = cv2.goodFeaturesToTrack(np.float32(img_gray), 100, 0.01, 10)
-		corners = np.int0(corners)
-		to_show_corners = img_copy.copy()
-		for corner in corners:
-			x, y = corner.ravel()
-			# to show corner
-			cv2.circle(to_show_corners, (x, y), 3, (80, 127, 255), 2)
-		return len(corners)
-
-	def get_harris_corners(self, img_copy, img_gray):
-		# get corners from corner harris
-		h_corners = cv2.cornerHarris(np.float32(img_gray), 2, 3, 0.04)
-		h_corners = np.int0(h_corners)
-		# object to show
-		to_show_corners_harris = img_copy.copy()
-		h_threshold = 0.05
-		for i in range(h_corners.shape[0]):
-			for j in range(h_corners.shape[1]):
-				if h_corners[i, j] > h_corners.max() * h_threshold:
-					cv2.circle(to_show_corners_harris, (j, i), 1, (0, 0, 255), 1)
-		amount_h_corners = len(h_corners[h_corners > h_corners.max() * h_threshold])
-		return amount_h_corners
-
-	def thresholding_image(self, img_gray, threshold):
-		_, img_thresh = cv2.threshold(img_gray, threshold, 255, cv2.THRESH_BINARY)
-		kernel = np.ones((3, 3), np.uint8)
-		img_thresh = cv2.morphologyEx(img_thresh, cv2.MORPH_OPEN, kernel, iterations=2)
-		return img_thresh
+		self.list_of_features.append(features_extracted)
 
 	def extract_to_table(self, labels, fmt="a"):
 		le = LabelEncoder()
@@ -156,12 +83,11 @@ class FeatureExtraction:
 			dict_writer.writeheader()
 			for dat in self.list_of_features:
 				dict_writer.writerow(dat)
-			# dict_writer.writerows(self.csv_data)
 
-
-class ImageToArrayPreprocessor:
-	def __init__(self, dataFormat=None):
-		self.dataFormat = dataFormat
-
-	def preprocess(self, image):
-		return img_to_array(image, data_format=self.dataFormat)
+	def extract_to_panda(self, labels):
+		le = LabelEncoder()
+		labels = le.fit_transform(labels)
+		for val in self.list_of_features:
+			val["labels"] = labels[self.list_of_features.index(val)]
+		self.data_frame = pd.DataFrame.from_dict(self.list_of_features)
+		return self.data_frame
